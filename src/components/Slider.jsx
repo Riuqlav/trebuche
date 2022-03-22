29bdf38bff482edd2e07d836f88344bfc4692242
Riuqlav <valquirsolza.vs@gmail.com>
2022-03-22 16:24:57 +0100

    Fix CSS slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,15 +1,22 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import React from "react";
+import styledComponents from "styled-components";
 import classes from "./Slider.module.css";
+
+// Used styled components because the module wouldn't work with props, nor inline
+const Arrow = styledComponents.div`
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+`;
 
 const Slider = (props) => {
   return (
     <div className={classes.Container}>
       <div className={classes.Arrow}>
-        <ArrowLeftOutlined />
+        <ArrowLeftOutlined direction="left" />
       </div>
       <div className={classes.Arrow}>
-        <ArrowRightOutlined />
+        <ArrowRightOutlined direction="right" />
       </div>
     </div>
   );
