79570fe22cebe4332ed4d91cf57b0fae52992cd3
Riuqlav <valquirsolza.vs@gmail.com>
2022-03-25 15:23:25 +0100

    image and info

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,21 +34,39 @@ const Wrapper = styled.div`
 `;
 
 const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
 `;
 
 const ImgContainer = styled.div`
+  height: 100%;
   flex: 1;
 `;
 
 const Image = styled.img`
+  height: 80%;
   flex: 1;
+  padding-left: 50px;
+  padding-right: 0px;
 `;
 
 const InfoContainer = styled.div`
+  padding-right: 100px;
   flex: 1;
 `;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button``;
 
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
@@ -66,10 +84,16 @@ const Slider = () => {
         <ArrowLeftOutlined />
       </Arrow>
       <Wrapper>
-        <ImgContainer>
-          <Image src="https://i.ibb.co/XsdmR2c/1.png" />
-        </ImgContainer>
-        <InfoContainer></InfoContainer>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f0b2e7d3-f9e7-455a-8f77-a7a9ae25dedc/d9wlxq3-634f227c-49fa-4fbf-80e0-16ba4f633937.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2YwYjJlN2QzLWY5ZTctNDU1YS04Zjc3LWE3YTlhZTI1ZGVkY1wvZDl3bHhxMy02MzRmMjI3Yy00OWZhLTRmYmYtODBlMC0xNmJhNGY2MzM5MzcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.r35G1doVRUVyd33kFGAa6pAPQou6VkjGjIYaLde0HqQ" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE!</Title>
+            <Desc>DON'T KILL YOURSELF YET, SUMMER DRESS SEASON IS HERE</Desc>
+            <Button>SHOW ME MORE</Button>
+          </InfoContainer>
+        </Slide>
       </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
