c1b6fb3f6143815cc16469ff98215c5b25c58e6e
Riuqlav <valquirsolza.vs@gmail.com>
2022-03-25 11:39:13 +0100

    Auto completion Css fixd

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,25 +1,80 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
-import React from "react";
-import styledComponents from "styled-components";
-import classes from "./Slider.module.css";
+import { useState } from "react";
+import styled from "styled-components";
 
-// Used styled components because the module wouldn't work with props, nor inline
-const Arrow = styledComponents.div`
-  left: ${(props) => props.direction === "left" && "10px"};
-  right: ${(props) => props.direction === "right" && "10px"};
-  alig
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
 `;
 
-const Slider = (props) => {
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  flex: 1;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+`;
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
   return (
-    <div className={classes.Container}>
-      <div className={classes.ArrowL}>
-        <ArrowLeftOutlined direction="left" />
-      </div>
-      <div className={classes.ArrowR}>
-        <ArrowRightOutlined direction="right" />
-      </div>
-    </div>
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+        </ImgContainer>
+        <InfoContainer></InfoContainer>
+      </Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
   );
 };
 
