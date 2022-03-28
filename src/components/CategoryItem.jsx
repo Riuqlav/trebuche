6687274ae0e3d40e23f3882e7bbd195e6072a0c9
Riuqlav <valquirsolza.vs@gmail.com>
2022-03-28 12:45:44 +0200

    Biulding cards

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,9 +2,29 @@ import React from "react";
 import { categories } from "../data";
 import styled from "styled-components";
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative; //for relative positioning of the image//
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const Info = styled.div`
+  position: absolute; //for showing on top of the image//
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Title = styled.h1``;
 const Button = styled.button``;
 
