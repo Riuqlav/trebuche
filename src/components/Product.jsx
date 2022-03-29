2d2568664e3400a278f5d0ebf3dbe38e28647aa0
Riuqlav <valquirsolza.vs@gmail.com>
2022-03-29 11:53:47 +0200

    Products finished

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,24 +5,63 @@ import {
 } from "@material-ui/icons";
 import styled from "styled-components";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  /* The images are not all PNG do NOT change background-color */
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  /* pop in and out in hover */
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
   min-width: 280px;
   height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* To make it change the backgrond on hover */
+  position: relative;
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
-const Circle = styled.div``;
 const Image = styled.img`
   height: 75%;
+  z-index: 2;
 `;
-const Info = styled.div``;
-const Icon = styled.div``;
 
 const Product = ({ item }) => {
   return (
     <Container>
-      <Circle />
       <Image src={item.img} />
       <Info>
         <Icon>
