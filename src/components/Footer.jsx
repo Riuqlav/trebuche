e41031f0d74eebf9a6d12d4f03022f1a83d48df8
Riuqlav <valquirsolza.vs@gmail.com>
2022-03-30 16:08:40 +0200

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,78 @@
+import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  /* paint the background color of each icon with the color props */
+  background-color: #${(props) => props.color};
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>
+          <h1>Boutique Ennuyeuse.</h1>{" "}
+        </Logo>
+        <Desc>
+          {" "}
+          This is a Dummy description for my first store website, since loren is
+          too basic I decided to write some corporate filling in here{" "}
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center></Center>
+      <Right></Right>
+    </Container>
+  );
+};
+
+export default Footer;
