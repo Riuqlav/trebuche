60774f964622dfb09f710b0aa62fb1a0367f7179
Riuqlav <valquirsolza.vs@gmail.com>
2022-04-06 13:42:07 +0200

    Editing Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import Cart from "./pages/Cart";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Cart />;
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/productlist" element={<ProductList />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 };
 
 export default App;
