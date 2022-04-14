b76c95dfb10821f00015bf9d332a754bcd0651cc
Riuqlav <valquirsolza.vs@gmail.com>
2022-04-14 16:00:27 +0200

    Hosting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   /*BrowserRouter needs to be here for it to work*/
-  <BrowserRouter>
-    <App />
+  <BrowserRouter basename={window.location.pathname || ""}>
+    <Routes>
+      <Route exact path="/" element={<App />}></Route>
+    </Routes>
   </BrowserRouter>,
   document.getElementById("root")
 );
